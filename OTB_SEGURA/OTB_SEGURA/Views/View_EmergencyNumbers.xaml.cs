57777e0077dc5bfce7ea39e5530bc2ae6b44bb45
pablo rodriguez <pablors0598@gmail.com>
2021-05-06 01:37:33 -0400

    se agrego los cambios de Miguel Terrazas a develop

--- OTB_SEGURA/OTB_SEGURA/Views/View_EmergencyNumbers.xaml.cs
+++ OTB_SEGURA/OTB_SEGURA/Views/View_EmergencyNumbers.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using OTB_SEGURA.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,32 @@ namespace OTB_SEGURA.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class View_EmergencyNumbers : ContentPage
     {
+        EmergencyNumbersViewModel emergencyNumbersList = new EmergencyNumbersViewModel();
         public View_EmergencyNumbers()
         {
             InitializeComponent();
+        }
+        private void list_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            try
+            {
+                ShowExitDialog(e);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error al marcar ", "intente nuevamente", "OK", "cancel");
+            }
+        }
+
+        private async void ShowExitDialog(SelectedItemChangedEventArgs e)
+        {
+            var number = emergencyNumbersList.emergencyNumbersList[e.SelectedItemIndex].Number;
+            var answer = await DisplayAlert("Llamar a " + emergencyNumbersList.emergencyNumbersList[e.SelectedItemIndex].Name, "¿Desea realizar la llamada?", "Aceptar", "Cancelar");
+
+            if (answer)
+            {
+                PhoneDialer.Open(number.ToString());
+            }
         }
     }
 }

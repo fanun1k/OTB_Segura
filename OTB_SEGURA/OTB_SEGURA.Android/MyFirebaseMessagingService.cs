aa42cb785ad7673623fda6d6aa9ffc1e8bfde634
mmmirko23 <mmmirko23@gmail.com>
2021-05-27 19:11:20 -0400

    implementacion de notificaciones push

---
+++ OTB_SEGURA/OTB_SEGURA.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OTB_SEGURA.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        [Obsolete]
+#pragma warning disable CS0809 // El miembro obsoleto invalida un miembro no obsoleto
+        public override void OnMessageReceived(RemoteMessage message)
+#pragma warning restore CS0809 // El miembro obsoleto invalida un miembro no obsoleto
+        {
+            base.OnMessageReceived(message);
+            SendNotification(message.GetNotification());
+        }
+
+        [Obsolete]
+        private void SendNotification(RemoteMessage.Notification message)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var defaultSoundUri = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+            var notificationBuilder = new NotificationCompat.Builder(this)
+                .SetSmallIcon(Resource.Drawable.logoOtb)
+                .SetContentTitle(message.Title)
+                .SetContentText(message.Body)
+                .SetAutoCancel(true)
+                .SetSound(defaultSoundUri)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}

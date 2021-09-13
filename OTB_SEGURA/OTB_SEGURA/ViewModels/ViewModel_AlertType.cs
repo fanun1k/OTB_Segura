6e57e71fc15bba72109cbca60efa3a1784ed2709
Mrochar98@gmail.com <Mrochar98@gmail.com>
2021-09-13 12:36:58 -0400

    Interfaz alertas de usuarios

---
+++ OTB_SEGURA/OTB_SEGURA/ViewModels/ViewModel_AlertType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OTB_SEGURA.ViewModels
+{
+     class ViewModel_AlertType:BaseViewModel
+    {
+        public ViewModel_AlertType( string title)
+        {
+            Title = title;
+        }
+    }
+}

e8553b22685cfdcd59237a2a750730c29ecc89af
Pablo Daniel Rodriguez Solis <pablors0598@gmail.com>
2021-10-24 11:14:16 -0400

    intentando consumir el videostream de kafka

--- OTB_SEGURA/OTB_SEGURA/ViewModels/SeeCameraViewModel.cs
+++ OTB_SEGURA/OTB_SEGURA/ViewModels/SeeCameraViewModel.cs
@@ -1,39 +1,100 @@
-﻿using GalaSoft.MvvmLight.Command;
+﻿using Confluent.Kafka;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 using System.Windows.Input;
 using Xamarin.Forms;
-using Amazon.KinesisVideo;
-using Amazon.KinesisVideo.Model;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace OTB_SEGURA.ViewModels
 {
     class SeeCameraViewModel : BaseViewModel
     {
-        AmazonKinesisVideoClient client = new AmazonKinesisVideoClient("AKIAQPXAFAEE2CTZI4VA", "8i0yx+xRpH3P32QrwyeAGLttAvEhGvDqeqU+gP3o", Amazon.RegionEndpoint.SAEast1);
-        List<Task<GetDataEndpointResponse>> respuestas = new List<Task<GetDataEndpointResponse>>();
+        ConsumerConfig conf;
+        private ImageSource imageVid;
+
+        public ImageSource ImageVid
+        {
+            get { return imageVid; }
+            set { imageVid = value;OnPropertyChanged(); }
+        }
+
+
+
+        public SeeCameraViewModel()
+        {
+            Title = "Ver Cámaras";
+
+            conf = new ConsumerConfig
+            {
+                GroupId = "web_consumer",
+                BootstrapServers = "192.168.0.100:29092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                //AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+        }
+
         public ICommand SeeCamera
         {
             get
             {
-                return new RelayCommand(async() => {
-                    GetDataEndpointRequest getDataEndpointRequest=new GetDataEndpointRequest();
-                    getDataEndpointRequest.StreamARN = "arn:aws:kinesisvideo:sa-east-1:033756086537:stream/CameraStream/1634161194699";
-                    getDataEndpointRequest.StreamName = "CameraStream";
-                    getDataEndpointRequest.APIName = APIName.GET_MEDIA;
-                    await Task.Run(() => {
-                        respuestas.Add(client.GetDataEndpointAsync(getDataEndpointRequest));
-                    }).ConfigureAwait(false);
-                    
-                
+                return new RelayCommand(() =>
+                {
+                    try
+                    {
+                        using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+                        {
+                            c.Subscribe("video-stream");
+
+                            CancellationTokenSource cts = new CancellationTokenSource();
+                            Console.CancelKeyPress += (_, e) =>
+                            {
+                                e.Cancel = true; // prevent the process from terminating.
+                                cts.Cancel();
+                            };
+                            while (true)
+                            {
+                                try
+                                {
+                                    var cr = c.Consume(cts.Token);
+                                    string key = cr.Message.Key == null ? "Null" : cr.Message.Key.ToString();
+                                    //ImageVid = cr.Message.Value;  //esto puede ser......
+                                    ImageVid = stringToImage(cr.Message.Value);
+                                    //Console.WriteLine($"Consumed message '{cr.Message}' at: '{cr.TopicPartitionOffset}'.");
+                                }
+                                catch (ConsumeException e)
+                                {
+                                    throw e;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        DependencyService.Get<IMessage>().LongAlert(ex.Message);
+                    }              
                 });
-                
+            
             }
 
+
         }
-        public SeeCameraViewModel()
+        public ImageSource stringToImage(string inputString)
         {
-            Title = "Ver Cámaras";
+            byte[] imageBytes = Encoding.Unicode.GetBytes(inputString);
+
+            // Don't need to use the constructor that takes the starting offset and length
+            // as we're using the whole byte array.
+            MemoryStream ms = new MemoryStream(imageBytes);
+
+            ImageSource image = ImageSource.FromStream(() => new MemoryStream(ms.ToArray()));
+
+            return image;
         }
     }
 }

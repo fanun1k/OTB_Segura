35adf55f93c5f7371ab1cff96d2d40da7ded3ba8
pablo rodriguez <pablors0598@gmail.com>
2021-11-15 10:53:52 -0400

    se agrego el codigo para detonar la alarma vecinal

---
+++ OTB_SEGURA/OTB_SEGURA/Views/View_EnableDisableAlarm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using OTB_SEGURA.Models;
+using OTB_SEGURA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OTB_SEGURA.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View_EnableDisableAlarm : ContentPage
+    {
+        public View_EnableDisableAlarm()
+        {
+            InitializeComponent();
+        }
+        public View_EnableDisableAlarm(AlarmModel alarma)
+        {
+            InitializeComponent();
+            BindingContext = new ViewModel_EnableDisableAlarm(alarma);
+        }
+    }
+}
